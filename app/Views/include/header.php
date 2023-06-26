f88f026fdce83f349984784a35727c1a27be328b
pankaj <pankaj.mandal@bsa.co.in>
2023-06-26 18:45:43 +0530

    update

--- app/Views/include/header.php
+++ app/Views/include/header.php
@@ -33,5 +33,5 @@
 
         <!-- Preloader -->
         <div class="preloader flex-column justify-content-center align-items-center">
-            <img class="animation__shake" src="dist/img/AdminLTELogo.png" alt="AdminLTELogo" height="60" width="60">
+            <img class="animation__shake" src="<?= base_url() ?>/theme/dist/img/AdminLTELogo.png" alt="AdminLTELogo" height="60" width="60">
         </div>
