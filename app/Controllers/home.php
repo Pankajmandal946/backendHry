5fdf9b4746265e8e64df2e9aeb238fdad9ad9310
pankaj <pankaj.mandal@bsa.co.in>
2023-06-26 17:35:14 +0530

    add new website in codegineter

--- app/Controllers/home.php
+++ app/Controllers/home.php
@@ -5,11 +5,11 @@ namespace App\Controllers;
 class home extends BaseController
 {
    public function Home(){
-    return view("include/header");
-    return view("include/navbar");     
-    return view("include/sidebar");
-    return view("backHry/home");
-    return view("include/footerJs");
-    return view("include/footer");
+    echo view("include/header");
+    echo  view("include/navbar");     
+    echo view("include/sidebar");
+    echo view("backHry/home");
+    echo view("include/footerJs");
+    echo view("include/footer");
    }
 }
