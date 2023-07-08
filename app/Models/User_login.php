0236eca61c5a22f409f2993fc5bb8cc9a1157cdd
pankaj <pankaj.mandal@bsa.co.in>
2023-07-08 19:03:05 +0530

    update  files

--- app/Models/User_login.php
+++ app/Models/User_login.php
@@ -29,13 +29,71 @@ class User_login extends Model
 
     function validate_login()
     {
-        $data = [
-            'username'  => $this->username
-        ];
-        $data1 = [
-            'is_active' => 1
-        ];
-        
+        $user = $this->username;
+        $is_act = 1;
+
+        $query   = $this->db->query("SELECT user_login_id, " . $this->table_name . ".user_id, name, email_id, mobile_no, username,user_type.user_type, password, default_password_change FROM " . $this->table_name . " INNER JOIN user ON (" . $this->table_name . ".user_id=user.user_id) INNER JOIN user_type ON (user.user_type_id=user_type.user_type_id) WHERE username = '$user' AND " . $this->table_name . ".is_active='$is_act'");
+        $row   = $query->getRowArray();
+        $this->db->close();
+        if(isset($row) && count($row)>0) {
+            if($this->validate_password($this->password, $row['password'])) {
+                // print_r($row);exit;
+                if($row['default_password_change']==1) {
+
+                    $user_login_id = $row['user_login_id'];
+                    $last_login_time = date('Y-m-d H:i:s');
+                    
+                    $data = [
+                        'user_login_id'     => $row['user_login_id'],
+                        'last_login_time'   => date('Y-m-d H:i:s')
+                    ];
+                    print_r($user_login_id);
+                    echo "<br>";
+                    print_r($last_login_time);exit;
+                    $query   = $this->db->query("UPDATE ".$this->table_name." SET last_login_time ='$last_login_time' WHERE user_login_id= '$user_login_id'");
+                    
+                    $this->db->close();
+                    
+                    session_start();
+                    $_SESSION["pdk_session_status"] = true;
+                    $_SESSION["pdk_user_id"]        = $row['user_id'];
+                    $_SESSION["pdk_user_login_id"]  = $row['user_login_id'];
+                    $_SESSION["pdk_name"]           = $row['name'];
+                    $_SESSION["pdk_email_id"]       = $row['email_id'];
+                    $_SESSION["pdk_mobile_no"]      = $row['mobile_no'];
+                    $_SESSION["pdk_user_type"]      = $row['user_type'];
+                    $_SESSION["pdk_username"]       = $row['username'];
+                    print_r($_SESSION);exit;
+                    return true;
+                } else {
+                    throw new \Exception('Please change your default password', 402);
+                }
+            } else {
+                throw new \Exception("Invalid Password",401);
+            }
+        }else {
+            throw new \Exception('User does not exists',401);
+        }
+    }
+
+    function validate_password($password='', $db_password='') {
+        if(password_verify($password, $db_password)) {
+            return true;
+        }
+        return false;
+    }
+}
+/*
+$stmt = $this->conn->prepare("SELECT user_login_id, " . $this->table_name . ".user_id, name, email_id, mobile_no, username,user_type.user_type, password, default_password_change FROM " . $this->table_name . " INNER JOIN user ON (" . $this->table_name . ".user_id=user.user_id) INNER JOIN user_type ON (user.user_type_id=user_type.user_type_id) WHERE username = :username AND " . $this->table_name . ".is_active=:is_active");
+        $stmt->execute($data);
+        $count = $stmt->rowCount();
+        echo $count;exit;
+        $row = $stmt->fetch();
+        $debug_query = $stmt->_debugQuery();
+        echo $debug_query;exit;
+        // print_r($row);exit;
+        $stmt->closeCursor();
+
         $builder = $this->db->table($this->table_name);
         $builder->select('user_login_id, "'.$this->table_name.'".user_id, name, email_id, mobile_no, username,user_type.user_type, password, default_password_change');
         $builder->from($this->table_name);
@@ -54,16 +112,11 @@ class User_login extends Model
         // // echo $debug_query;exit;
         // // print_r($row);exit;
         // $stmt->closeCursor();
-    }
-}
-/*
-$stmt = $this->conn->prepare("SELECT user_login_id, " . $this->table_name . ".user_id, name, email_id, mobile_no, username,user_type.user_type, password, default_password_change FROM " . $this->table_name . " INNER JOIN user ON (" . $this->table_name . ".user_id=user.user_id) INNER JOIN user_type ON (user.user_type_id=user_type.user_type_id) WHERE username = :username AND " . $this->table_name . ".is_active=:is_active");
-        $stmt->execute($data);
-        $count = $stmt->rowCount();
-        echo $count;exit;
-        $row = $stmt->fetch();
-        $debug_query = $stmt->_debugQuery();
-        echo $debug_query;exit;
-        // print_r($row);exit;
-        $stmt->closeCursor();
+
+
+        $sql = "UPDATE ".$this->table_name." SET last_login_time =:last_login_time WHERE user_login_id=:user_login_id";
+                    $stmt= $this->conn->prepare($sql);
+                    $stmt->execute($data);
+                    // print_r($stmt);exit; 
+                    $stmt->closeCursor();
 */
